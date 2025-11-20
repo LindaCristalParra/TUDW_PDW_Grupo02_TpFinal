9991291d4ce416cf519a63de59f705a9e0d1b8d0
Andrea Crespillo <andrea.crespillo@est.fi.uncoma.edu.ar>
2025-11-19 22:20:14 -0300

    Cambios en Panel

--- Vista/admin/panelAdmin.php
+++ Vista/admin/panelAdmin.php
@@ -24,8 +24,8 @@ if (!$isAdmin) {
     <h2>Panel de Administración</h2>
     <div class="list-group mt-3">
         <a href="/TUDW_PDW_Grupo02_TpFinal/Vista/admin/cargarProducto.php" class="list-group-item list-group-item-action">Cargar productos</a>
-        <a href="/TUDW_PDW_Grupo02_TpFinal/Vista/Estructura/Accion/Compra/listado.php" class="list-group-item list-group-item-action">Mis ventas / Listado de compras</a>
-        <a href="/TUDW_PDW_Grupo02_TpFinal/Vista/Estructura/Accion/Compra/listado.php?soloVentas=true" class="list-group-item list-group-item-action">Ventas (solo ventas)</a>
+        <a href="/TUDW_PDW_Grupo02_TpFinal/Vista/Estructura/Accion/Compra/listado.php" class="list-group-item list-group-item-action">Mis ventas</a>
+    
     </div>
 </div>
 
